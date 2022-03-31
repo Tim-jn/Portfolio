762d103bb4b76a374f2b2edf8574efa737aa7a3b
Tim <Tim280696@hotmail.com>
2022-03-31 14:55:08 +0200

    updated style

--- src/Components/SideBar.tsx
+++ src/Components/SideBar.tsx
@@ -20,28 +20,32 @@ export default function SideBar() {
       </div>
       <ul className="nav-list">
         <li>
-          <a href="#profile">
+          <a href="#about-me">
             <FontAwesomeIcon className="icons" icon={faAddressCard} />
-            <span className="link-name">Who am I ?</span>
+            <span className="link-name">About me</span>
           </a>
+          <span className="tooltip">About me</span>
         </li>
         <li>
           <a href="#skills">
             <FontAwesomeIcon className="icons" icon={faLaptopCode} />
             <span className="link-name">My hard skills</span>
           </a>
+          <span className="tooltip">My hard skills</span>
         </li>
         <li>
           <a href="#work">
             <FontAwesomeIcon className="icons" icon={faFolderOpen} />
             <span className="link-name">My work</span>
           </a>
+          <span className="tooltip">My work</span>
         </li>
         <li>
-          <a href="#work">
+          <a href="#contact">
             <FontAwesomeIcon className="icons" icon={faEnvelope} />
             <span className="link-name">Contact</span>
           </a>
+          <span className="tooltip">Contact</span>
         </li>
       </ul>
       <div className="profile-content">
