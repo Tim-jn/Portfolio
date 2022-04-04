2598b86de77b080a3579f3d334ac14394703c690
Tim <Tim280696@hotmail.com>
2022-04-04 15:10:35 +0200

    Added section + fixed nav

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,15 @@ import Title from './Components/Title/Title'
 function App() {
   return (
     <main className="App">
+      <SideBar />
       <section id="home" className="home">
         <SocialBar />
         <Title />
-        <SideBar />
       </section>
+      <section id="about-me" className="about"></section>
+      <section id="skills" className="skills"></section>
+      <section id="work" className="work"></section>
+      <section id="contact" className="contact"></section>
     </main>
   )
 }
