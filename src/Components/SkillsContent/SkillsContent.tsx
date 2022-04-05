0d8612169404eb566c6b662dfbb1192d12c6c441
Tim <Tim280696@hotmail.com>
2022-04-05 16:15:47 +0200

    updated skills content

--- src/Components/SkillsContent/SkillsContent.tsx
+++ src/Components/SkillsContent/SkillsContent.tsx
@@ -4,14 +4,32 @@ import ChartSkills from '../ChartSkills/ChartSkills'
 export default function SkillsContent() {
   return (
     <div className="skills-content">
-      <h2 className="skills-title">
-        <span className="skills-title-letter-capital">S</span>
-        <span className="skills-title-letter">k</span>
-        <span className="skills-title-letter">i</span>
-        <span className="skills-title-letter">l</span>
-        <span className="skills-title-letter">l</span>
-        <span className="skills-title-letter">s</span>
-      </h2>
+      <div className="skills-text">
+        <h2 className="skills-title">
+          <span className="skills-title-letter-capital">S</span>
+          <span className="skills-title-letter">k</span>
+          <span className="skills-title-letter">i</span>
+          <span className="skills-title-letter">l</span>
+          <span className="skills-title-letter">l</span>
+          <span className="skills-title-letter">s</span>
+        </h2>
+        <div className="skills-paragraphs">
+          <p className="skills-paragraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+            pariatur ex debitis necessitatibus blanditiis repellendus laboriosam
+            fugit, similique adipisci nobis! Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Dolor, sed. Iure praesentium suscipit
+            id eum amet, corporis expedita error blanditiis.
+          </p>
+          <p className="skills-paragraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+            pariatur ex debitis necessitatibus blanditiis repellendus laboriosam
+            fugit, similique adipisci nobis! Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Dolor, sed. Iure praesentium suscipit
+            id eum amet, corporis expedita error blanditiis.
+          </p>
+        </div>
+      </div>
       <div className="chart-skills">
         <ChartSkills />
       </div>
