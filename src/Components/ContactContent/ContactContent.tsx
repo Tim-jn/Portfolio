5c11d48fc9b37d385b7daa87f680eff3c7a25df1
Tim <Tim280696@hotmail.com>
2022-04-08 12:11:23 +0200

    Updated

--- src/Components/ContactContent/ContactContent.tsx
+++ src/Components/ContactContent/ContactContent.tsx
@@ -6,7 +6,7 @@ export default function ContactContent() {
     <div className="contact-content">
       <div className="contact-text">
         If you want to work with me or if you have any questions, feel free to
-        contact me by mail or linkedin. I will answer you as soon as possible.
+        contact me by mail or linkedin. I will answer as soon as possible.
       </div>
       <div className="contact-buttons">
         <Button
