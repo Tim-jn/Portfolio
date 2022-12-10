8a90b95aab05e61ffeb80ab5e6348f0d7351399c
Tim <Tim280696@hotmail.com>
2022-12-10 23:16:18 +0100

    redesign home content

--- src/Components/HomeContent/HomeContent.tsx
+++ src/Components/HomeContent/HomeContent.tsx
@@ -1,123 +1,22 @@
 import Button from '../Button/Button'
 import './HomeContent.scss'
 import { useMediaQuery } from 'react-responsive'
+import SocialBar from '../SocialBar/SocialBar'
 
 export default function Title() {
   const isMobile = useMediaQuery({ query: '(max-width: 634px)' })
 
   return (
-    <div className="main-title">
-      <h1 className="main-title-content">
-        <span className="title-letter first-capital">H</span>
-        <span className="title-letter" data-content="i">
-          i
-        </span>
-        <span className="title-letter" data-content=",">
-          ,
-        </span>
-        <br />
-        <span className="title-letter" data-content="I">
-          I
-        </span>
-        <span className="title-letter" data-content="'">
-          '
-        </span>
-        <span className="title-letter letter-spaced" data-content="m">
-          m
-        </span>
-        <span className="title-letter" data-content="T">
-          T
-        </span>
-        <span className="title-letter" data-content="i">
-          i
-        </span>
-        <span className="title-letter" data-content="m">
-          m
-        </span>
-        <span className="title-letter" data-content=",">
-          ,
-        </span>
-        <br />
-        <span className="title-letter letter-spaced" data-content="a">
-          a
-        </span>
-        <span className="title-letter" data-content="w">
-          w
-        </span>
-        <span className="title-letter" data-content="e">
-          e
-        </span>
-        <span className="title-letter letter-spaced" data-content="b">
-          b
-        </span>
-        {isMobile ? (
-          <div className="text-developer">
-            <span className="title-letter" data-content="d">
-              d
-            </span>
-            <span className="title-letter" data-content="e">
-              e
-            </span>
-            <span className="title-letter" data-content="v">
-              v
-            </span>
-            <span className="title-letter" data-content="e">
-              e
-            </span>
-            <span className="title-letter" data-content="l">
-              l
-            </span>
-            <span className="title-letter" data-content="o">
-              o
-            </span>
-            <span className="title-letter" data-content="p">
-              p
-            </span>
-            <span className="title-letter" data-content="e">
-              e
-            </span>
-            <span className="title-letter" data-content="r">
-              r
-            </span>
-          </div>
-        ) : (
-          <>
-            <span className="title-letter" data-content="d">
-              d
-            </span>
-            <span className="title-letter" data-content="e">
-              e
-            </span>
-            <span className="title-letter" data-content="v">
-              v
-            </span>
-            <span className="title-letter" data-content="e">
-              e
-            </span>
-            <span className="title-letter" data-content="l">
-              l
-            </span>
-            <span className="title-letter" data-content="o">
-              o
-            </span>
-            <span className="title-letter" data-content="p">
-              p
-            </span>
-            <span className="title-letter" data-content="e">
-              e
-            </span>
-            <span className="title-letter" data-content="r">
-              r
-            </span>
-          </>
-        )}
-      </h1>
-      <Button
-        className="button contact-btn"
-        text="Contact me"
-        href="#contact"
-        target="_self"
-      />
+    <div className="title-box">
+      <div className="title-box-content">
+        <SocialBar />
+        <h1 className="main-title">
+          <div className="title-name">TIM JEANMART</div>
+          <div className="title-text">Développeur Front-End</div>
+        </h1>
+        <div className="border-box"></div>
+      </div>
+      <div className="arrow-down"></div>
     </div>
   )
 }
