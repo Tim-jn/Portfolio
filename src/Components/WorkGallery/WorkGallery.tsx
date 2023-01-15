c3654ba73a4265f5a8727c61441861390dc37230
Tim <Tim280696@hotmail.com>
2023-01-15 17:38:45 +0100

    add modal

--- src/Components/WorkGallery/WorkGallery.tsx
+++ src/Components/WorkGallery/WorkGallery.tsx
@@ -1,109 +1,122 @@
 import './WorkGallery.scss'
 // import Button from '../Button/Button'
-import Fylo from './work-images/fylo.webp'
-import Jeanmartth from './work-images/jeanmartth.webp'
-import Shortly from './work-images/shortly.webp'
-import Rimarok from './work-images/rimarok.webp'
+import Fylo from './work-images/fylo.svg'
+import Jeanmartth from './work-images/jeanmartth.svg'
+import Shortly from './work-images/shortly.svg'
+import Rimarok from './work-images/rimarok.svg'
+import PrevRimarok from './work-images/rimarok.webp'
+import PrevJeanmartth from './work-images/jeanmartth.webp'
+import PrevFylo from './work-images/fylo.webp'
+import PrevShortly from './work-images/shortly.webp'
+import { useState } from 'react'
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import {
 //   faChevronLeft,
 //   faChevronRight,
 // } from '@fortawesome/free-solid-svg-icons'
 
+const data = [
+  {
+    id: 1,
+    name: 'Rimarok.com',
+    image: Rimarok,
+    preview: PrevRimarok,
+    live: 'https://rimarok.com/',
+    description:
+      'Refonte du site web professionel https://rimarok.com en partenariat avec Romaric Ruga, développeur Fullstack freelance',
+    languages: 'React, TypeScript, JAMStack, Bootstrap/SCSS',
+  },
+  {
+    id: 2,
+    name: 'Shortly',
+    image: Shortly,
+    preview: PrevShortly,
+    live: 'https://timjn-shortly.netlify.app',
+    source: 'https://github.com/Tim-jn/shortly',
+    description:
+      'This project is part of the Frontend Mentor challenges. Your challenge is to build out this URL shortening API landing page and get it looking as close to the design as possible.',
+    languages: 'React, TypeScript and Sass',
+  },
+  {
+    id: 3,
+    name: 'Jeanmartth.be',
+    image: Jeanmartth,
+    preview: PrevJeanmartth,
+    live: 'https://jeanmartth.be',
+    description:
+      "This project is a website for a heating company. It was built to develop my wordpress skills. There's no source code for this project.",
+    languages: 'WordPress',
+  },
+  {
+    id: 4,
+    name: 'Fylo',
+    image: Fylo,
+    preview: PrevFylo,
+    live: 'https://timjn-fylo.netlify.app/',
+    source: 'https://github.com/Tim-jn/Fylo',
+    description:
+      'This project is part of the Frontend Mentor challenges. Your challenge is to build out this landing page and get it looking as close to the design as possible.',
+    languages: 'React, TypeScript and Sass',
+  },
+]
+
 export default function WorkGallery() {
-  const data = [
-    {
-      name: 'Rimarok.com',
-      image: Rimarok,
-      live: 'https://rimarok.com/',
-      description:
-        'Redesign of the website https://rimarok.com in partnership with Romaric Ruga.',
-      languages: 'React, TypeScript, JAMStack, Bootstrap/SCSS',
-    },
-    {
-      name: 'Shortly',
-      image: Shortly,
-      live: 'https://timjn-shortly.netlify.app',
-      source: 'https://github.com/Tim-jn/shortly',
-      description:
-        'This project is part of the Frontend Mentor challenges. Your challenge is to build out this URL shortening API landing page and get it looking as close to the design as possible.',
-      languages: 'React, TypeScript and Sass',
-    },
-    {
-      name: 'Jeanmartth.be',
-      image: Jeanmartth,
-      live: 'https://jeanmartth.be',
-      description:
-        "This project is a website for a heating company. It was built to develop my wordpress skills. There's no source code for this project.",
-      languages: 'WordPress',
-    },
-    {
-      name: 'Fylo',
-      image: Fylo,
-      live: 'https://timjn-fylo.netlify.app/',
-      source: 'https://github.com/Tim-jn/Fylo',
-      description:
-        'This project is part of the Frontend Mentor challenges. Your challenge is to build out this landing page and get it looking as close to the design as possible.',
-      languages: 'React, TypeScript and Sass',
-    },
-  ]
+  const [showModal, setShowModal] = useState(false)
+  const [active, setActive] = useState<any | null>(null)
+
+  function getClass(index: Number) {
+    return index === active?.id ? 'active' : 'inactive'
+  }
+
+  const Modal = ({ object }: any) => (
+    <div className="work-modal">
+      <img src={object.preview} alt={object.name} />
+      <span className="description">{object.description}</span>
+      <div className="close" onClick={() => setShowModal(false)}></div>
+    </div>
+  )
 
   return (
     <div className="gallery-content">
       <h2 className="work-title">Projets récents</h2>
       <div className="work-content">
-        {data.map((item, key) => {
+        {data.map(({ id, name, preview, image, description }) => {
           return (
-            <div key={key} className={`work-item-${key + 1}`}>
-              <div className="work-miniature">
-                {/* <div className="slide-work">
-                  <Button
-                    className="button work-button"
-                    text="Live preview"
-                    href={item.live}
-                    target="_blank"
-                  />
-                  {item.source ? (
-                    <Button
-                      className="button work-button"
-                      text="Source code"
-                      href={item.source}
-                      target="_blank"
-                    />
-                  ) : (
-                    ''
-                  )}
-                </div> */}
-                <p className="site-name">{item.name}</p>
-                <p className="more-info">Plus d'infos</p>
-                {/* <img
-                    src={item.image}
-                    alt={item.name + ' project'}
+            <>
+              <div
+                key={id}
+                className={`work-item work-item-${id} ` + getClass(id)}
+                onClick={() => {
+                  setActive({ id, name, preview, image, description })
+                  setShowModal(true)
+                }}
+              >
+                <div className="work-miniature">
+                  <p className="more-info">plus d'infos ?</p>
+                  <img
+                    src={image}
+                    alt={name + ' project'}
                     className="work-image"
-                  /> */}
+                  />
+                </div>
               </div>
-              {/* <div className="work-text-content">
-              <h3 className="work-text-title">
-                <FontAwesomeIcon
-                  className="work-title-icon"
-                  icon={faChevronLeft}
-                />
-                {item.name}
-                <span className="slash">/</span>
-                <FontAwesomeIcon
-                  className="work-title-icon"
-                  icon={faChevronRight}
-                />
-              </h3>
-              <p className="work-text-description">{item.description}</p>
-              <p className="work-text-languages">
-                I used {item.languages} to create this project.
-              </p>
-              <p className="more-infos">
-                Get more infos by hovering the thumbnail.
-              </p>
-            </div> */}
-            </div>
+              {showModal ? <Modal object={active} /> : null}
+              {/* {showModal ? (
+                <div
+                  className={
+                    !setShowModal
+                      ? `work-modal`
+                      : `work-modal work-modal-active`
+                  }
+                >
+                  <img src={image} alt={description} />
+                  <div
+                    className="close"
+                    onClick={() => setShowModal(false)}
+                  ></div>
+                </div>
+              ) : null} */}
+            </>
           )
         })}
       </div>
@@ -111,7 +124,7 @@ export default function WorkGallery() {
         <hr />
         <br />
         <div className="more-work-text">
-          Vous voulez voir plus de projets ? Cliquez sur le lien ci-dessous !
+          Vous voulez voir plus de projets ? Cliquez sur le bouton ci-dessous !
         </div>
         <button className="see-more">
           <a
